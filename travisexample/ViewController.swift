43339601570e1b5b4955d5bc4578bbb02a531163
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-06-15 22:00:46 +0300

    Initial Commit

---
+++ travisexample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  travisexample
+//
+//  Created by Gokhan Gultekin on 15.06.2018.
+//  Copyright © 2018 Gokhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
