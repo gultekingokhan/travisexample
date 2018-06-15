565e8ddcb7ab1f1a20eed7148bd368fedda45ca1
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-06-15 22:18:59 +0300

    Test functions added

--- travisexampleTests/travisexampleTests.swift
+++ travisexampleTests/travisexampleTests.swift
@@ -21,16 +21,27 @@ class travisexampleTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func test_addition() {
+        
+        let a = 2
+        let b = 3
+        
+        XCTAssertEqual(a+b, 5, "Test failed when adding two numbers.")
+    }
+    
+    func test_subtraction() {
+        
+        let a = 5
+        let b = 2
+        
+        XCTAssertEqual(a-b, 3, "Test failed when subtracting two numbers")
     }
     
 }
